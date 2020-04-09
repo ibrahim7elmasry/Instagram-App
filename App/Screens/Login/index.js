8804e7c77f83062a65d054cb9d5963c61ace38ae
ibrahim7elmasry <ibrahimelmasry777@gmail.com>
2020-04-09 02:48:54 +0200

    Login Screen

--- App/Screens/Login/index.js
+++ App/Screens/Login/index.js
@@ -1,21 +1,55 @@
 import React, { Component } from 'react'
-import { Text, View , TouchableOpacity} from 'react-native'
-import MyTaps from './../../Navigation/index';
-  
+import {
+     Text,
+      View , 
+      ScrollView,
+      TouchableOpacity,
+      Image,
+      TextInput,
+      Button
+    } from 'react-native'
+
+import styles from './styles'  
 export default class Login extends Component {
+    state = {
+        email: '',
+        password: '',
+    };
     render() {
         return (
-            <View>
-                <Text> Login </Text>
-
-                <TouchableOpacity 
-                    onPress={()=>{
-                        this.props.navigation.navigate("Tabs")
-                    }}
-                >
-                    <Text> Click </Text>
-                </TouchableOpacity>
+         <ScrollView style={styles.root}>
+          <View style={styles.Logo}>
+              <Image
+                 source={require('../../Images/instagram.png')}
+                 style={{ width:100 ,height:100 ,}} />
             </View>
+            
+            <View style={styles.textInputs}>
+                 <TextInput
+                    placeholder="Email"
+                    value={this.state.email}
+                    onChangeText={text =>this.setState({email:text})}
+                     type="email"
+                      style={styles.email_password_textInputs}/>  
+
+              <TextInput
+                    placeholder="password"
+                    secureTextEntry={true}
+                    value={this.state.password}
+                    onChangeText={ (password) => this.setState({ password }) }
+                    type="password"
+                    style={styles.email_password_textInputs}/>  
+                     </View>
+ 
+        <View style={styles.loginSubmit}>
+                <Button
+                 title='Login'
+                 color="#f95e62"
+                  onPress={()=>{ this.props.navigation.navigate("Tabs")}} />
+              </View>
+
+
+            </ScrollView>
         )
     }
 }
--- App/Screens/Login/styles.js
+++ App/Screens/Login/styles.js
@@ -1,9 +1,25 @@
-import { Themes } from './../Constants'
-export default {
+ export default {
      root: {
-    backgroundColor: Colors.basic.whiteTwo,
+    backgroundColor:'white',
     flex:1
   },
+  Logo:{
+       alignItems: 'center',
+        marginVertical: 80,
+   },
+   textInputs:{
+    flexDirection:'column', 
+    justifyContent: 'center',
+     padding:30, 
+    },
+    email_password_textInputs:{
+        backgroundColor: '#e5e5e5',
+        height: 40,
+         margin:10
+    },
+    loginSubmit: {
+     marginHorizontal: 100,
+      },
   
   
 }
